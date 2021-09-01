459b35e4e304aa9704bb00906a7fab3a8234ebca
fabricioig863 <fabricioig863@gmail.com>
2021-09-01 18:04:14 -0300

    improving shopping cart with redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { Provider } from 'react-redux'
 import Catalog from './components/Catalog'
+
 import store from './store'
+import Cart from './components/Cart'
 
 export default function App(){
   return(
     <Provider store={store}>
       <Catalog />
+      <Cart />
     </Provider>
   )
 }
--- src/components/Catalog.tsx
+++ src/components/Catalog.tsx
@@ -1,13 +1,41 @@
-import { useSelector } from "react-redux"
+import { useState, useEffect, useCallback  } from "react"
+import { useDispatch } from "react-redux"
+import api from "../services/api"
+import { addProductToCart } from "../store/modules/cart/actions"
+import { IProduct } from "../store/modules/cart/types"
+
 
 export default function Catalog() {
-  const catalog = useSelector(state => state)
+  const dispatch = useDispatch()
+  const [catalog, setCatalog] = useState<IProduct[]>([])
+  
+  useEffect(() => {
+    api.get('products').then(response => {
+      setCatalog(response.data)
+    })
+  }, [])
 
-  console.log(catalog)
+  const handleAddProductToCart = useCallback((product: IProduct) => {
+    dispatch(addProductToCart(product))
+  }, [dispatch])
 
   return(
-    <div>
+    <main>
      <h1>Catalog</h1>
-    </div>
+     {catalog.map((product) => (
+       <article key={product.id}>
+        <strong>{product.id}</strong>
+        <strong>{product.title}</strong> {" - "}
+        <strong>{product.price}</strong> {" - "}
+
+        <button 
+          type="button"
+          onClick={() => handleAddProductToCart(product)}
+        >
+          Comprar
+        </button>
+       </article>
+     ))}
+    </main>
   )
 }
